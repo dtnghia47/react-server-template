87596dee2a5569c4e686a50cc8e01900f6e69983
dtnghia47 <dtnghia47@gmail.com>
2021-10-14 22:18:32 +0700

    init repo nextjs

---
+++ pages/post/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const PostPage: NextPage = () => {
+  return <div>
+    <Head>
+      <title>Post Page</title>
+      <meta name="description" content="Post page description" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>Post Page</h1>
+  </div>
+}
+
+export default PostPage;
